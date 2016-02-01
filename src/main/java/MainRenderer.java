285be4c525148e2c15ee44ac08df828efe4cefb6
Henry Wilkes <hw17g12@soton.ac.uk>
2016-02-01 13:56:51 +0000

    Can now render cube

--- src/main/java/MainRenderer.java
+++ src/main/java/MainRenderer.java
@@ -1,6 +1,7 @@
 package main.java;
 import main.java.objects.RenderableObject;
 import main.java.objects.Vertex3;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
@@ -8,13 +9,16 @@ import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
 
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class MainRenderer {
 
-    private List<RenderableObject> objs;
+    //private List<RenderableObject> objs;
+
+    private FloatBuffer vertices;
 
     private long lastFPS;
     private long fps;
@@ -24,8 +28,8 @@ public class MainRenderer {
     private float yrotation = 0;
     private float zrotation = 0;
 
-    private final float xrotationspeed = 0.1f;
-    private final float yrotationspeed = 1f;
+    private final float xrotationspeed = 0.01f;
+    private final float yrotationspeed = 0.01f;
     private final float zrotationspeed = 0.1f;
 
 
@@ -34,125 +38,39 @@ public class MainRenderer {
     }
 
     private void setupVertices(){
-        objs = new ArrayList<RenderableObject>();
+        vertices = BufferUtils.createFloatBuffer(3 * 4 * 6);
 
-        Vertex3[] v = new Vertex3[24];
-//        v[0] = new Vertex3(0,0,0);
-//        v[1] = new Vertex3(100,0,0);
-//        v[2] = new Vertex3(100,100,0);
-//        v[3] = new Vertex3(0,100,0);
-//
-//        v[4] = new Vertex3(0,100,100);
-//        v[5] = new Vertex3(0,100,0);
-//        v[6] = new Vertex3(0,0,0);
-//        v[7] = new Vertex3(0,0,100);
-//
-//        v[8] = new Vertex3(0,0,0);
-//        v[9] = new Vertex3(100,0,0);
-//        v[10] = new Vertex3(100,0,100);
-//        v[11] = new Vertex3(0,0,100);
-//
-//        v[12] = new Vertex3(0,100,100);
-//        v[13] = new Vertex3(0,100,0);
-//        v[14] = new Vertex3(100,100,0);
-//        v[15] = new Vertex3(100,1000,100);
-//
-//        v[16] = new Vertex3(100,100,0);
-//        v[17] = new Vertex3(100,0,0);
-//        v[18] = new Vertex3(100,0,100);
-//        v[19] = new Vertex3(100,100,100);
-//
-//        v[20] = new Vertex3(0,100,100);
-//        v[21] = new Vertex3(0,0,100);
-//        v[22] = new Vertex3(100,0,100);
-//        v[23] = new Vertex3(100,100,100);
-//
-         Vertex3[] c = new Vertex3[24];
-//        c[0] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[1] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[2] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[3] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[4] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[5] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[6] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[7] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[8] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[9] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[10] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[11] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[12] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[13] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[14] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[15] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[16] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[17] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[18] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[19] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[20] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[21] = new Vertex3(1.0f,1.0f,0.0f);
-//        c[22] = new Vertex3(0.0f,0.0f,0.0f);
-//        c[23] = new Vertex3(1.0f,1.0f,0.0f);
+        vertices.put(new float[] {
+                -1.0f, -1.0f, 1.0f,
+                1.0f, -1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f, 1.0f,
 
-        c[0] = new Vertex3(1.0f,1.0f,0.0f);
-        c[1] = new Vertex3(1.0f,1.0f,0.0f);
-        c[2] = new Vertex3(1.0f,1.0f,0.0f);
-        c[3] = new Vertex3(1.0f,1.0f,0.0f);
-        v[0] = new Vertex3(1.0f, 1.0f,-1.0f);
-        v[1] = new Vertex3(-1.0f, 1.0f,-1.0f);
-        v[2] = new Vertex3(-1.0f, 1.0f, 1.0f);
-        v[3] = new Vertex3( 1.0f, 1.0f, 1.0f);
+                -1.0f, -1.0f, -1.0f,
+                -1.0f, 1.0f, -1.0f,
+                1.0f, 1.0f, -1.0f,
+                1.0f, -1.0f, -1.0f,
 
+                -1.0f, 1.0f, -1.0f,
+                -1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, -1.0f,
 
-        c[4] = new Vertex3(1.0f,0.5f,0.0f);
-        c[5] = new Vertex3(1.0f,0.5f,0.0f);
-        c[6] = new Vertex3(1.0f,0.5f,0.0f);
-        c[7] = new Vertex3(1.0f,0.5f,0.0f);
-        v[4] = new Vertex3( 1.0f,-1.0f, 1.0f);
-        v[5] = new Vertex3(-1.0f,-1.0f, 1.0f);
-        v[6] = new Vertex3(-1.0f,-1.0f,-1.0f);
-        v[7] = new Vertex3( 1.0f,-1.0f,-1.0f);
+                -1.0f, -1.0f, -1.0f,
+                1.0f, -1.0f, -1.0f,
+                1.0f, -1.0f, 1.0f,
+                -1.0f, -1.0f, 1.0f,
 
-        c[8] = new Vertex3(1.0f,0.5f,0.0f);
-        c[9] = new Vertex3(1.0f,0.5f,0.0f);
-        c[10] = new Vertex3(1.0f,0.5f,0.0f);
-        c[11] = new Vertex3(1.0f,0.5f,0.0f);
-        v[8] = new Vertex3( 1.0f, 1.0f, 1.0f);
-        v[9] = new Vertex3(-1.0f, 1.0f, 1.0f);
-        v[10] = new Vertex3(-1.0f,-1.0f, 1.0f);
-        v[11] = new Vertex3( 1.0f,-1.0f, 1.0f);
+                1.0f, -1.0f, -1.0f,
+                1.0f, 1.0f, -1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, -1.0f, 1.0f,
 
-        c[12] = new Vertex3(1.0f,1.0f,0.0f);
-        c[13] = new Vertex3(1.0f,1.0f,0.0f);
-        c[14] = new Vertex3(1.0f,1.0f,0.0f);
-        c[15] = new Vertex3(1.0f,1.0f,0.0f);
-        v[12] = new Vertex3( 1.0f,-1.0f,-1.0f);
-        v[13] = new Vertex3(-1.0f,-1.0f,-1.0f);
-        v[14] = new Vertex3(-1.0f, 1.0f,-1.0f);
-        v[15] = new Vertex3( 1.0f, 1.0f,-1.0f);
+                -1.0f, -1.0f, -1.0f,
+                -1.0f, -1.0f, 1.0f,
+                -1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f, -1.0f});
 
-        c[16] = new Vertex3(1.0f,1.0f,0.0f);
-        c[17] = new Vertex3(1.0f,1.0f,0.0f);
-        c[18] = new Vertex3(1.0f,1.0f,0.0f);
-        c[19] = new Vertex3(1.0f,1.0f,0.0f);
-        v[16] = new Vertex3(-1.0f, 1.0f, 1.0f);
-        v[17] = new Vertex3(-1.0f, 1.0f,-1.0f);
-        v[18] = new Vertex3(-1.0f,-1.0f,-1.0f);
-        v[19] = new Vertex3(-1.0f,-1.0f, 1.0f);
-
-        c[20] = new Vertex3(1.0f,0.0f,1.0f);
-        c[21] = new Vertex3(1.0f,0.0f,1.0f);
-        c[22] = new Vertex3(1.0f,0.0f,1.0f);
-        c[23] = new Vertex3(1.0f,0.0f,1.0f);
-        v[20] = new Vertex3( 1.0f, 1.0f,-1.0f);
-        v[21] = new Vertex3( 1.0f, 1.0f, 1.0f);
-        v[22] = new Vertex3( 1.0f,-1.0f, 1.0f);
-        v[23] = new Vertex3( 1.0f,-1.0f,-1.0f);
-
-        Vertex3[] t = new Vertex3[24];
-
-        RenderableObject ob = new RenderableObject(v,c,t,400,300);
-
-        objs.add(ob);
     }
 
     private void updateFPS() {
@@ -183,83 +101,64 @@ public class MainRenderer {
         return delta;
     }
 
-//    private void render() {
+    private void render() {
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); // Clear The Screen And The Depth Buffer
+        GL11.glPushMatrix();
+        //GL11.glLoadIdentity(); // Reset The Current Modelview Matrix
+
+        GL11.glTranslatef(0.0f, 0.0f, -5);
+        GL11.glRotatef(xrotation, 1.0f, 0.0f, 0.0f); // Rotate On The X Axis
+        GL11.glRotatef(yrotation, 0.0f, 1.0f, 0.0f); // Rotate On The Y Axis
+        GL11.glRotatef(zrotation, 0.0f, 0.0f, 1.0f); // Rotate On The Z Axis
+        GL11.glColor3f(1.0f, 1.0f, 1.0f);
+
+        // Enable vertex arrays
+        GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
+        // Setup vertex array pointer
+        vertices.rewind();
+        GL11.glVertexPointer(3, 0, vertices);
+        // Draw using vertex array
+        GL11.glDrawArrays(GL11.GL_QUADS, 0, 24);
+        // Disable vertex arrays
+        GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY);
+        GL11.glPopMatrix();
+    }
+
+
+//    private void render(){
 //        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-//        GL11.glLoadIdentity();
 //
-//        GL11.glTranslatef(0f,0.0f,-7f);
-//        GL11.glRotatef(45f,0.0f,1.0f,0.0f);
-//        GL11.glColor3f(0.5f,0.5f,1.0f);
+//        GL11.glColor3f(0.5f, 0.5f, 1.0f);
 //
-//        GL11.glBegin(GL11.GL_QUADS);
-//        GL11.glColor3f(1.0f,1.0f,0.0f);
-//        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
-//        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
-//        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
-//        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
-//        GL11.glColor3f(1.0f,0.5f,0.0f);
-//        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
-//        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
-//        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
-//        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
-//        GL11.glColor3f(1.0f,0.0f,0.0f);
-//        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
-//        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
-//        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
-//        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
-//        GL11.glColor3f(1.0f,1.0f,0.0f);
-//        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
-//        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
-//        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
-//        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
-//        GL11.glColor3f(0.0f,0.0f,1.0f);
-//        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
-//        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
-//        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
-//        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
-//        GL11.glColor3f(1.0f,0.0f,1.0f);
-//        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
-//        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
-//        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
-//        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
-//        GL11.glEnd();
+//        Iterator<RenderableObject> iter = objs.iterator();
+//        Vertex3[] v;
+//        Vertex3[] c;
+//        RenderableObject o;
+//
+//
+//        while(iter.hasNext()) {
+//            GL11.glPushMatrix();
+//            o = iter.next();
+//
+//            GL11.glTranslatef(400, 200, 0);
+//            GL11.glRotatef(45f,0.0f,1.0f,0.0f);
+//            //GL11.glRotatef(yrotationspeed, 0f, 1f, 0f);
+//            //GL11.glRotatef(zrotationspeed, 0f, 0f, 1f);
+//
+//            v = o.getVertices();
+//            c = o.getColors();
+//            GL11.glBegin(GL11.GL_QUADS);
+//
+//            for(int i = 0; i < v.length; i++){
+//                GL11.glVertex3f(v[i].one,v[i].two,v[i].three);
+//                GL11.glColor3f(c[i].one,c[i].two,c[i].three);
+//            }
+//            GL11.glEnd();
+//
+//            GL11.glPopMatrix();
+//        }
 //
 //    }
-
-    private void render(){
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-
-        GL11.glColor3f(0.5f, 0.5f, 1.0f);
-
-        Iterator<RenderableObject> iter = objs.iterator();
-        Vertex3[] v;
-        Vertex3[] c;
-        RenderableObject o;
-
-
-        while(iter.hasNext()) {
-            GL11.glPushMatrix();
-            o = iter.next();
-
-            GL11.glTranslatef(400, 200, 0);
-            GL11.glRotatef(45f,0.0f,1.0f,0.0f);
-            //GL11.glRotatef(yrotationspeed, 0f, 1f, 0f);
-            //GL11.glRotatef(zrotationspeed, 0f, 0f, 1f);
-
-            v = o.getVertices();
-            c = o.getColors();
-            GL11.glBegin(GL11.GL_QUADS);
-
-            for(int i = 0; i < v.length; i++){
-                GL11.glVertex3f(v[i].one,v[i].two,v[i].three);
-                GL11.glColor3f(c[i].one,c[i].two,c[i].three);
-            }
-            GL11.glEnd();
-
-            GL11.glPopMatrix();
-        }
-
-    }
 
     private void loop(){
         while (!Display.isCloseRequested()) {
@@ -305,11 +204,6 @@ public class MainRenderer {
             System.exit(0);
         }
 
-        // init OpenGL
-//        GL11.glMatrixMode(GL11.GL_PROJECTION);
-//        GL11.glLoadIdentity();
-//        GL11.glOrtho(0, 1024, 0, 768, 1, -1);
-//        GL11.glMatrixMode(GL11.GL_MODELVIEW);
         InitGL();
 
         getDelta();
